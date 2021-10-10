c7d36d141721cab6787b3a7ab350a1cdfe2319bf
Razvan Rotaru <razvanrtr@outlook.com>
2021-10-10 21:36:17 +0300

    add setup tools package

---
+++ Editor/ToolsMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class ToolsMenu
+    {
+        [MenuItem("PJV/Setup/Create Default Folders")]
+        public static void CreateDefaultFolderHierarchy()
+        {
+            CreateDirectories("", "Scripts", "Resources");
+            CreateDirectories("Resources", "Prefabs", "Models", "Sprites");
+            AssetDatabase.Refresh();
+        }
+
+
+        private static void CreateDirectories(string root, params string[] directories)
+        {
+            var fullPath = Path.Combine(Application.dataPath, root);
+
+            foreach (var directory in directories)
+            {
+                Directory.CreateDirectory(Path.Combine(fullPath, directory));
+            }
+        }
+    }
+}
